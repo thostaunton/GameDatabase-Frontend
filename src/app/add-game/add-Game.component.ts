affb0e54220a4b59333f01ac3265e0ea4b746c5e
Primiister <primii3ter@hotmail.com>
2020-08-19 14:23:30 +0100

    Got forms working

--- src/app/add-game/add-Game.component.ts
+++ src/app/add-game/add-Game.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'add-game',
-  templateUrl: './add-game.component.html',
+  templateUrl: './add-game.component.html'
 })
-export class AddGameComponent {
+export class AddGameComponent implements OnInit{
 
   constructor(private http: HttpClient){}
+
     onSubmit(data){
-      console.warn(data);
-      this.http.post('http://localhost:8080/createGame', data).subscribe((result) =>{
-        console.warn("result", result);
+      console.log(this.title);
+      this.ngOnInit();
+
+
+      const game:JSON = <JSON><unknown>{
+        "title": this.title,
+        "genre": this.genre,
+        "publisher": this.publisher,
+        "release": this.release,
+        "reviewScore": this.reviewScore
+      }
+
+      console.log(game);
+
+
+      this.http.post('http://localhost:8080/createGame', game, {responseType: 'text'})
+      .subscribe((result) =>{
+        console.log("result", result);
       })
+    }
+
+    ngOnInit(){
+
     }
   }
