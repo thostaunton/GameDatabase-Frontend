84eee71112ca3379fb7ba0237115edee1bd3da50
Primiister <primii3ter@hotmail.com>
2020-08-20 23:42:53 +0100

    Updated table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import{ VideogameService } from './videogame.service';
+import{ IVideogame } from './videogame';
 
 @Component({
   selector: 'table',
   templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
 
+data = [];
+
+constructor(private _videogameService : VideogameService){}
+
+  ngOnInit() {
+
+    this._videogameService.getVideogames().subscribe(res => {
+      this.data = res;
+    });
+  }
 }
---
+++ src/app/table/videogame.service.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import{ IVideogame } from './videogame';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class VideogameService {
+  constructor(private http: HttpClient){}
+  private _url : string = 'http://localhost:8080/getAllGames';
+
+  getVideogames(): Observable<IVideogame[]>{
+    return this.http.get<IVideogame[]>(this._url);
+  }
+}
